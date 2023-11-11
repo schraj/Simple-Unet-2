834876b326886864a450c86b0b56516a13284ec1
Jeremy Schrader <jeschrader@paypal.com>
2023-11-11 12:42:01 -0800

    t

--- src/model_api/trainer.py
+++ src/model_api/trainer.py
@@ -74,19 +74,12 @@ class Trainer:
         check_accuracy(val_loader, self.model, device=h.DEVICE)
         scaler = torch.cuda.amp.GradScaler()
         
-        best_score = 0
         for epoch in range(h.NUM_EPOCHS):
             print("Epoch ",epoch)
             self.train_fn(train_loader, optimizer, loss_fn, scaler)
 
             check_accuracy(val_loader, self.model, device=h.DEVICE)
-            
-            # if current_score > best_score:
-            #     best_score = current_score 
-            #     save_predictions_as_imgs(
-            #         val_loader, self.model, folder="saved_images", device=h.DEVICE
-            #     )
-            
+                        
             if epoch % 10 == 0:
                 self.modelLifecyle.save_model()
                 print("Model saved")
